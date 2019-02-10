7c5a116002998b24c6ac885b3516115ad06adcbf
Patric Pereira <patric.pereirafranca@gmail.com>
2019-02-10 21:33:24 -0200

    criando cenas de OutrosJogos e SobreJogo

---
+++ src/components/outrosJogos.android.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export default class OutrosJogos extends Component {
+  render() {
+    return (
+      <Text style={{ flex: 1, backgroundColor: '#61bd8c' }}>Aqui podem ser apresentadas informações sobre outros jogo do desenvolvedor</Text>
+    );
+  }
+}
---
+++ src/components/sobreJogo.android.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export default class SobreJogo extends Component {
+  render() {
+    return (
+      <Text style={{ flex: 1, backgroundColor: '#61bd8c' }}>Aqui podem ser apresentadas informações sobre o jogo</Text>
+    );
+  }
+}
