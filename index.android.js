60d2cf47dd0eae64ad53600801cc9693ecff4f30
Patric Pereira <patric.pereirafranca@gmail.com>
2019-02-10 18:45:10 -0200

    criando a cena principal

--- index.android.js
+++ index.android.js
@@ -1,53 +1,49 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image
 } from 'react-native';
+
+const btnJogar = require('./src/img/botao_jogar.png');
+const btnSobreJogo = require('./src/img/sobre_jogo.png');
+const btnOutrosJogos = require('./src/img/outros_jogos.png');
+const logo = require('./src/img/logo.png');
 
 export default class app6 extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+      <View style={styles.principal}>
+        <View style={styles.apresentacao}>
+          <Image source={logo} />
+          <Image source={btnJogar} />
+        </View>
+        <View style={styles.rodape}>
+          <Image source={btnSobreJogo} />
+          <Image source={btnOutrosJogos} />
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  principal: {
     flex: 1,
+    backgroundColor: '#61bd8c'
+  },
+  apresentacao: {
+    flex: 10,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    alignItems: 'center'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  rodape: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
 });
 
 AppRegistry.registerComponent('app6', () => app6);
