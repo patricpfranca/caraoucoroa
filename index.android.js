9cb46ebc09ac376c6b3f0c067a55e03051b7333f
Patric Pereira <patric.pereirafranca@gmail.com>
2019-02-10 21:59:09 -0200

    incluindo routers

--- index.android.js
+++ index.android.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
-import {
-  AppRegistry
-} from 'react-native';
+import { AppRegistry } from 'react-native';
+import { Router, Scene } from 'react-native-router-flux';
+
+import Principal from './src/components/principal.android';
+import SobreJogo from './src/components/sobreJogo.android';
+import OutrosJogos from './src/components/outrosJogos.android';
 
 export default class app6 extends Component {
   render() {
-    return ();
+    return (
+      <Router>
+        <Scene key='app'>
+          <Scene key='principal' component={Principal} initial title='Cara ou coroa' />
+          <Scene key='sobrejogo' component={SobreJogo} />
+          <Scene key='outrosjogos' component={OutrosJogos} />
+        </Scene>
+      </Router>
+    );
   }
 }
 
